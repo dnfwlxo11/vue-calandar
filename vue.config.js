ab0fe91be20a2facadbe83478bb119cec91b870f
dnfwlxo11 <dnfwlxo11@naver.com>
2023-01-25 15:46:40 +0900

    edit: git pages deploy setting change

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   outputDir: path.resolve(__dirname, "./docs"),
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/vue-calendar",
   // css: {
   //   extract: false,
   // },
